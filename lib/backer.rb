bc68533a7b8a2d9109bcb9eb8e4258fef21f7429
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-19 04:40:55 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer
   attr_reader :name, :backed_projects
-  attr_accessor :project
+  attr_accessor :backers
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
 
-  def back_project(project)
+  def back_project(project, name)
     @backed_projects << project
-
+    @backers << name
   end
 end
