eb707de611a1d06dbf3b10d1f57c292caebbbb16
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-19 05:17:11 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,6 +7,7 @@ class Project
   end
 
   def add_backer(backer)
-    @backers << backer.back_project(self) unless backer.backed_projects.include?(self)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 end
